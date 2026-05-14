4c1a24bd2e0142974f496b7f3ab10a40d50a53ff
seongjun1011 <sj297916@gmail.com>
2026-05-14 15:02:25 +0900

    탭호스트테스트

--- app/src/main/java/kr/ac/kopo/taphosttest/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/taphosttest/MainActivity.java
@@ -12,26 +12,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        // 1. getTabHost() 대신 findViewById를 사용하세요.
         TabHost tabHost = findViewById(android.R.id.tabhost);
-        // 2. 반드시 setup()을 먼저 호출해야 함!
+
+        // 2. 반드시 setup()을 호출해야 합니다.
         tabHost.setup();
 
+        // 고양이 탭
+        TabHost.TabSpec tabSpecCat = tabHost.newTabSpec("cat").setIndicator("고양이");
+        tabSpecCat.setContent(R.id.tabCat);
+        tabHost.addTab(tabSpecCat);
 
-        TabHost.TabSpec tabSpecMusic = tabHost.newTabSpec("music").setIndicator("음악");
-        tabSpecMusic.setContent(R.id.tabMusic);
-        tabHost.addTab(tabSpecMusic);
+        // 개 탭
+        TabHost.TabSpec tabSpecDog = tabHost.newTabSpec("dog").setIndicator("개");
+        tabSpecDog.setContent(R.id.tabDog);
+        tabHost.addTab(tabSpecDog);
 
+        // 토끼 탭
+        TabHost.TabSpec tabSpecRabbit = tabHost.newTabSpec("rabbit").setIndicator("토끼");
+        tabSpecRabbit.setContent(R.id.tabRabbit);
+        tabHost.addTab(tabSpecRabbit);
 
-        TabHost.TabSpec tabSpecArtist = tabHost.newTabSpec("artist").setIndicator("가수");
-        tabSpecArtist.setContent(R.id.tabArtist);
+        // 말 탭 (태그를 "cat"에서 "horse"로 수정했습니다)
+        TabHost.TabSpec tabSpecHorse = tabHost.newTabSpec("horse").setIndicator("말");
+        tabSpecHorse.setContent(R.id.tabHorse);
+        tabHost.addTab(tabSpecHorse);
 
-
-        TabHost.TabSpec tabSpecAlbum = tabHost.newTabSpec("album").setIndicator("앨범");
-        tabSpecAlbum.setContent(R.id.tabAlbum);
-        tabHost.addTab(tabSpecAlbum);
-
-
+        // (선택사항) 시작할 때 보여줄 탭 설정
         tabHost.setCurrentTab(0);
     }
 }
